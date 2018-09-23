8d48e9a40f982120b6b81eba5a2c05d8f146e3a6
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:39:35 +0700

    Integral

---
+++ src/if3/pkg10117095/latihan7/integral/IF310117095Latihan7Integral.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan7.integral;
+
+/**
+ *
+ * @author 
+ *  NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi contoh penulisan dengan type data Integer
+ */
+public class IF310117095Latihan7Integral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+        
+        
+    }
+    
+}
